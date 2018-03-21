18ce403403bdb4dc2692bddbacdbd128d999fe67
Arpit Panda <f2014114@pilani.bits-pilani.ac.in>
2018-03-22 02:35:55 +0530

    Delay Accomodated

--- network.py
+++ network.py
@@ -15,9 +15,9 @@ import genetic
 #             s1------s5-------s6------s4
 #
 
-a = 1
-b = 1
-c = 1
+# a = 1
+# b = 1
+# c = 1
 
 def is_valid_Path(path,network):
 
@@ -29,7 +29,7 @@ def is_valid_Path(path,network):
 
     return 1
 
-def get_fitness(path,bandwidth,network):
+def get_fitness(path,bandwidth,delay,network):
 
     min = -1
     connectedNodes = 0
@@ -51,18 +51,30 @@ def get_fitness(path,bandwidth,network):
         isvalidpath = 1
     else:
         isvalidpath = 0
-    return Fitness(connectedNodes,min,isvalidpath);
+    total_delay = 0
+    if isvalidpath:
+        for x in range(0,len(path)-1):
+            total_delay+=delay[path[x]][path[x+1]]
+    else:
+        total_delay = 5000
+
+    return Fitness(connectedNodes,min,total_delay,isvalidpath);
 
 class Fitness:
 
     connectedNodes = 0
     bandwidth = 0
     isValidPath = -1
+    delay = 0
+    jitter = 0
 
-    def __init__(self,connectedNodes,bandwidth,isValidPath):
+
+    def __init__(self,connectedNodes,bandwidth,delay,isValidPath):
         self.connectedNodes = connectedNodes
         self.bandwidth = bandwidth
         self.isValidPath = isValidPath
+        self.delay = delay
+        # self.jitter = jitter
 
 
 
@@ -150,7 +162,17 @@ class Chromosome:
         return True
 
 
-
+a=0.5
+b=0.5
+req_bw = 4000
+req_delay = 4
+def calculateFitness(cx):
+    x = (a * ((cx.fitness.delay - req_delay) / req_delay))
+    y = (b * ((req_bw - cx.fitness.bandwidth) / float (req_bw)))
+    valx = (a * ((cx.fitness.delay - req_delay) / float (req_delay))) + (b *  (req_bw - cx.fitness.bandwidth)  / float(req_bw) )
+    print("b = "+str(b)+" cx.fitness.bandwidth= "+str(cx.fitness.bandwidth)+" req_bandwidth= "+str(req_bw)+" "+" val= "+str(y))
+    print("a = "+ str(a) + " cx.fitness.delay= " + str(cx.fitness.delay) + " req_delay= " + str(req_delay) + " " + " val= " + str(x))
+    return valx
 
 def cmp(cx, cy):
     if cx.fitness.isValidPath < cy.fitness.isValidPath:
@@ -159,10 +181,11 @@ def cmp(cx, cy):
         return -1
     elif cx.fitness.isValidPath == cy.fitness.isValidPath:
         if cx.fitness.isValidPath == 1:
-            if cx.fitness.bandwidth < cy.fitness.bandwidth:
-                return 1
-            else:
+
+            if calculateFitness(cx) < calculateFitness(cy):
                 return -1
+            else:
+                return 1
         else:
             if cx.fitness.connectedNodes < cy.fitness.connectedNodes:
                 return 1
@@ -195,76 +218,160 @@ class Network:
             network[dst] = []
             network[dst].append(src)
 
+    def add_delay(self,delay,src,dst,t):
+        delay.setdefault(src,{})
+        delay.setdefault(dst,{})
+        delay[src][dst] = t
+        delay[dst][src] = t
+
     def test_network(self):
         network = {}
         network['switches'] = []
-        network['switches']=['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13','s14']
+        # network['switches']=['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13','s14']
+        # self.add_link(network,'n1','s1')
+        # self.add_link(network,'s1','s2')
+        # self.add_link(network,'s2','s3')
+        # self.add_link(network,'s3','s4')
+        # self.add_link(network,'s4','n2')
+        #
+        # self.add_link(network,'s1','s3')
+        # self.add_link(network,'s2','s4')
+        #
+        # self.add_link(network,'s1','s5')
+        # self.add_link(network,'s5','s6')
+        # self.add_link(network,'s6','s4')
+        #
+        # self.add_link(network, 's2', 's7')
+        # self.add_link(network, 's7', 's8')
+        # self.add_link(network, 's8', 'n2')
+        #
+        # self.add_link(network, 's9', 'n1')
+        # self.add_link(network, 's9', 's2')
+        # self.add_link(network, 's9', 's1')
+        #
+        # self.add_link(network, 'n1', 's10')
+        # self.add_link(network, 's10','s11')
+        # self.add_link(network, 's11', 'n2')
+        #
+        # self.add_link(network, 's14', 's13')
+        # self.add_link(network, 's13', 's12')
+        #
+        #
+        # bandwidth = {}
+        #
+        # bandwidth['s1'] = 6000
+        # bandwidth['s2'] = 4000
+        # bandwidth['s3'] = 500
+        # bandwidth['s4'] = 500
+        # bandwidth['s5'] = 3000
+        # bandwidth['s6'] = 3000
+        # bandwidth['s7'] = 4000
+        # bandwidth['s8'] = 4000
+        # bandwidth['s9'] = 5000
+        # bandwidth['s10'] = 6000
+        # bandwidth['s11'] = 6000
+        # bandwidth['s12'] = 6000
+        # bandwidth['s13'] = 6000
+        # bandwidth['s14'] = 6000
+        #
+        # delay = {}
+        #
+        # delay['n1'] = {}
+        # delay['s1'] = {}
+        # delay['s2'] = {}
+        # delay['s3'] = {}
+        # delay['s4'] = {}
+        # delay['s5'] = {}
+        # delay['s6'] = {}
+        # delay['s7'] = {}
+        # delay['s8'] = {}
+        # delay['s9'] = {}
+        # delay['s10'] = {}
+        # delay['s11'] = {}
+        # delay['s12'] = {}
+        # delay['s13'] = {}
+        # delay['s14'] = {}
+
+        network['switches'] = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8']
+
         self.add_link(network,'n1','s1')
+        self.add_link(network,'n1','s6')
+        self.add_link(network,'n1','s4')
         self.add_link(network,'s1','s2')
         self.add_link(network,'s2','s3')
-        self.add_link(network,'s3','s4')
-        self.add_link(network,'s4','n2')
+        self.add_link(network,'s3','n2')
 
-        self.add_link(network,'s1','s3')
-        self.add_link(network,'s2','s4')
+        self.add_link(network, 's4', 'n1')
+        self.add_link(network, 's4', 's5')
+        self.add_link(network, 's5', 's3')
 
-        self.add_link(network,'s1','s5')
-        self.add_link(network,'s5','s6')
-        self.add_link(network,'s6','s4')
-
-        self.add_link(network, 's2', 's7')
+        self.add_link(network, 's6', 'n1')
+        self.add_link(network, 's6', 's7')
         self.add_link(network, 's7', 's8')
         self.add_link(network, 's8', 'n2')
-
-        self.add_link(network, 's9', 'n1')
-        self.add_link(network, 's9', 's2')
-        self.add_link(network, 's9', 's1')
-
-        self.add_link(network, 'n1', 's10')
-        self.add_link(network, 's10','s11')
-        self.add_link(network, 's11', 'n2')
-
-        self.add_link(network, 's14', 's13')
-        self.add_link(network, 's13', 's12')
-
 
         bandwidth = {}
 
         bandwidth['s1'] = 6000
         bandwidth['s2'] = 4000
-        bandwidth['s3'] = 500
-        bandwidth['s4'] = 500
+        bandwidth['s3'] = 4000
+        bandwidth['s4'] = 2000
         bandwidth['s5'] = 3000
-        bandwidth['s6'] = 3000
+        bandwidth['s6'] = 4000
         bandwidth['s7'] = 4000
         bandwidth['s8'] = 4000
-        bandwidth['s9'] = 5000
-        bandwidth['s10'] = 6000
-        bandwidth['s11'] = 6000
-        bandwidth['s12'] = 6000
-        bandwidth['s13'] = 6000
-        bandwidth['s14'] = 6000
+
+        delay = {}
+
+        delay['n1'] = {}
+        delay['s1'] = {}
+        delay['s2'] = {}
+        delay['s3'] = {}
+        delay['s4'] = {}
+        delay['s5'] = {}
+        delay['s6'] = {}
+        delay['s7'] = {}
+        delay['s8'] = {}
+
+        self.add_delay(delay,'n1','s1',2)
+        self.add_delay(delay, 's1', 's2', 2)
+        self.add_delay(delay, 's2', 's3', 2)
+        self.add_delay(delay, 's3', 'n2', 2)
+
+        self.add_delay(delay, 'n1', 's6', 3)
+        self.add_delay(delay, 's6', 's7', 0.5)
+        self.add_delay(delay, 's7', 's8', 0.5)
+        self.add_delay(delay, 's8', 'n2', 0.5)
+
+        self.add_delay(delay, 'n1', 's4', 0.5)
+        self.add_delay(delay, 's4', 's5', 0.5)
+        self.add_delay(delay, 's5', 's3', 0.5)
+
+
+
 
         paths = []
         path = ['n1', 's2', 's1','n2']
         paths.append(path)
-        path = ['n1', 's2', 's1', 's3', 's4', 'n2']
+        path = ['n1', 's2', 's1', 's3', 'n2']
         paths.append(path)
-        path = ['n1', 's1', 's2', 's3', 's4', 's5', 'n2']
+        path = ['n1', 's7', 's1', 's8', 'n2']
         paths.append(path)
-        path = ['n1', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 'n2']
+        path = ['n1', 's4', 's2', 's6', 'n2']
         paths.append(path)
-        path = ['n1', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 'n2']
+        path = ['n1', 's1', 's2', 's8', 'n2']
         paths.append(path)
-        path = ['n1', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 'n2']
-        paths.append(path)
+        # path = ['n1', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 'n2']
+        # paths.append(path)
+        # path = ['n1', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 'n2']
+        # paths.append(path)
 
-        path = ['n1', 's1', 's2', 's3', 's4', 'n2']
+        # path = ['n1', 's1', 's2', 's3', 's4', 'n2']
 
         parentPool = []
 
         for path in paths:
-            parentPool.append(Chromosome(path,get_fitness(path,bandwidth,network),0,1))
+            parentPool.append(Chromosome(path,get_fitness(path,bandwidth,delay,network),0,1))
 
         parentPool.sort(cmp)
 
@@ -279,7 +386,7 @@ class Network:
         # strategy = 1 => mutation
         # strategy = 2 => crossover
 
-        maxGen = 100
+        maxGen = 300
 
         gen = 1
 
@@ -300,7 +407,7 @@ class Network:
                     child = mutate(parent.genes,network)
                     if child not in pathsPool:
                         if not isrepetitivePath(child):
-                            childPool.append(Chromosome(child,get_fitness(child,bandwidth,network),1,gen))
+                            childPool.append(Chromosome(child,get_fitness(child,bandwidth,delay,network),1,gen))
                 else:
                     x = random.randrange(0,len(parentPool))
                     y = random.randrange(0,len(parentPool))
@@ -311,7 +418,7 @@ class Network:
                     for path in paths:
                         if path not in pathsPool:
                             if not isrepetitivePath(path):
-                                childPool.append(Chromosome(path,get_fitness(path,bandwidth,network),2,gen))
+                                childPool.append(Chromosome(path,get_fitness(path,bandwidth,delay,network),2,gen))
 
 
             parentPool.sort(cmp)
@@ -332,6 +439,8 @@ class Network:
 
             for parent in parentPool:
                 display(parent)
+                if(parent.fitness.isValidPath):
+                    print("Fitness value : "+str(calculateFitness(parent)) )
 
             gen = gen + 1
 
